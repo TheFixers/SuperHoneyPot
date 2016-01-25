e9ff96c3e1fb06cb6fb115b1fc2db6843e6699a3
Fred Montoya <fredmontoya22@yahoo.com>
2016-01-25 15:59:56 -0700

    telnet-reader.py basically listens on port 23 for telnet connections then closes. Potential to add more interaction later.

---
+++ telnet-reader.py
@@ -0,0 +1,39 @@
+import socket, threading
+
+HOST = ''
+PORT = 23
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+s.listen(4)
+clients = [] #list of clients connected
+lock = threading.Lock()
+
+
+class chatServer(threading.Thread):
+    def __init__(self, (socket,address)):
+        threading.Thread.__init__(self)
+        self.socket = socket
+        self.address= address
+
+    def run(self):
+        lock.acquire()
+        clients.append(self)
+        lock.release()
+        print '%s:%s connected.' % self.address
+        # while True:
+        #     data = self.socket.recv(1024)
+        #     if not data:
+        #         break
+        #     for c in clients:
+        #         c.socket.send(data)
+        self.socket.close()
+        print '%s:%s disconnected.' % self.address
+        lock.acquire()
+        clients.remove(self)
+        lock.release()
+
+while True: # wait for socket to connect
+    # send socket to chatserver and start monitoring
+    chatServer(s.accept()).start()
