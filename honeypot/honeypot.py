9363b9c5d21986212010921f2dbe0c6936ba7fec
Fred Montoya <fredmontoya22@yahoo.com>
2016-01-26 13:57:18 -0700

    added ssl adition to the one I have been working on. Removed extra .idea project which was interfeering with the previous one.

--- honeypot/honeypot.py
+++ honeypot/honeypot.py
@@ -1,9 +1,11 @@
 from src import telnet_reader
 from src import http_reader
 from src import https_reader
+from src import ssh_plugin
 
 
 while 1:
     telnet_reader.Server(telnet_reader.s.accept()).start()
     https_reader.Server(https_reader.s.accept()).start()
     http_reader.Server(http_reader.s.accept()).start()
+    ssh_plugin.Server(ssh_plugin.s.accept()).start()
