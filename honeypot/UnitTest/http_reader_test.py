3c18d116e616bba04c0e0246df6f2b177e6b57c0
fred15 <fredmontoya22@gmail.com>
2016-02-04 16:48:12 -0700

    another because

--- honeypot/UnitTest/http_reader_test.py
+++
@@ -1,9 +0,0 @@
-import os
-path = os.path.dirname(os.path.realpath(__file__)).replace("UnitTest", "plugins")
-
-import sys
-sys.path.insert(0, path)
-
-from http_reader import http_server_plugin, web_server_handler
-
-server = http_server_plugin(8080)/
