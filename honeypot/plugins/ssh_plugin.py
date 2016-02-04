02ba5487940292fa7f8af126a9e3f260bf4fc40a
Mikhail Batkalin <mbatkali@msudenver.edu>
2016-02-03 19:57:26 -0700

    pulled from newest branch

---
+++ honeypot/plugins/ssh_plugin.py
@@ -0,0 +1,35 @@
+#__author__ = 'daemoniclegend'
+
+import time
+import socket
+import threading
+
+HOST = ''
+PORT = 22
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+s.listen(4)
+lock = threading.Lock()
+
+
+class Server(threading.Thread):
+
+    track_data = []
+
+    def __init__(self, (socket, address)):
+        threading.Thread.__init__(self)
+        self.socket = socket
+        self.address = address
+        self.time = time
+
+    def run(self):
+        print 'Connection with %s:%s' % self.address
+        self.time = time.ctime(time.time())
+        self.socket.send('Login as: ')
+
+        self.track_data = [self.time, self.address, self.socket, PORT]
+
+
+
