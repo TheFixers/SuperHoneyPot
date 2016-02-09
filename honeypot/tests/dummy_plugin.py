86747f797afa452c255e1dea4dc69d2638480155
raddack <raddack@gmail.com>
2016-02-09 12:58:26 -0700

    Removed __init__ since it bad practice to have in unittest folder rename Dummy_client.py to dummy_plugin.py
    Worked on more loader_test still a working progress

--- honeypot/tests/dummy_plugin.py
+++ honeypot/tests/dummy_plugin.py
@@ -1,5 +1,6 @@
 import socket
 import sys
+import threading
 
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -10,25 +11,36 @@ sock.bind(server_address)
 # Listen for incoming connections
 sock.listen(1)
 
-while True:
-    # Wait for a connection
-    print  'waiting for a connection'
-    connection, client_address = sock.accept()
-    try:
-        print >>sys.stderr, 'connection from', client_address
+class server(threading.Thread):
+    def __init__(self, (socket,address)):
+        threading.Thread.__init__(self)
+        self.socket = socket
+        self.address = address
 
-        # Receive the data in small chunks and retransmit it
-       # while True:
-        #    data = connection.recv(16)
-         #   print 'received "%s"' % data
-         #   if data:
-          #      print , 'sending data back to the client'
-           #     connection.sendall(data)
-           # else:
-            #    print 'no more data from', client_address
-            #    break
 
-    finally:
-        # Clean up the connection
-        connection.close()
-        break
+    while True:
+        # Wait for a connection
+        print  'waiting for a connection'
+        connection, client_address = sock.accept()
+        try:
+            print >>sys.stderr, 'connection from', client_address
+
+            # Receive the data in small chunks and retransmit it
+           # while True:
+            #    data = connection.recv(16)
+             #   print 'received "%s"' % data
+             #   if data:
+              #      print , 'sending data back to the client'
+               #     connection.sendall(data)
+               # else:
+                #    print 'no more data from', client_address
+                #    break
+
+        finally:
+            # Clean up the connection
+            connection.close()
+            break
+
+
+if __name__ == "__main__":
+    pass
--- honeypot/tests/loader_test.py
+++ honeypot/tests/loader_test.py
@@ -1,41 +1,88 @@
-import dummy_plugin
 import threading
 import socket
 import unittest
 import os, imp
 imp.load_source("loader", os.path.join(os.path.dirname(__file__), "..\honey_loader\loader.py"))
 import loader
+import dummy_plugin #not sure what this running on
+import dummy_client
+'''
+    Since import dummy_plugin runs the plugin. You need to run
+
+
+'''
+
 
 class TestLoader(unittest.TestCase):
 
     # Test the method start_plugin(class_to_run, socket_to_run): with good inputs
+    # Excepted that no errors occurs and a valid connection is made.
     def test_startplugin(self):
-        threading._start_new_thread(loader.start_plugin,(dummy_plugin.Server, dummy_plugin.s))
-
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
+            threading._start_new_thread(loader.start_plugin,(dummy_plugin.server, dummy_plugin.sock))
+
             HOST = 'localhost'    # The remote host
             PORT = 23              # The same port as used by the server
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
             s.connect((HOST, PORT))
+
         except Exception as e:
             self.fail("The connection was not made")
         finally:
             s.close()
-        self.assertTrue(False)
 
-    #This will check the start plugin is really setting the ports or not.
-    #def test_startpluginincorrectport(self):
-     #   loader.start_plugin(dummy_plugin.Server,  24)
-      #  try:
-       #     HOST = ''    # The remote host
-        #    PORT = 23              # The same port as used by the server
-         #   s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-          #  s.connect((HOST, HOST))
-       # except Exception as e:
-        #    self.fail("The connection was not made")
-        #finally:
-         #   s.close()
-         #   self.assertTrue(True)
+    # Test the method start_plugin(class_to_run, socket_to_run): with an default socket input
+    # Excepted no error to occur and connection will be made
+    def test_startplugin_invalidsocket(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        try:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            server_address = ('localhost', 80)
+            sock.bind(server_address)
+            sock.listen(1)
+            threading._start_new_thread(loader.start_plugin,(dummy_plugin.server, sock))
+
+            HOST = 'localhost'    # The remote host
+            PORT = 80              # The same port as used by the server
+
+            s.connect((HOST, PORT))
+        except Exception as e:
+            self.fail("A connection was not made")
+        finally:
+            s.close()
+
+    # Test the method start_plugin(class_to_run, socket_to_run): with a null inputs
+    # Excepted AttributeError since socket_to_run does not have .accept()
+    def test_startplugin_nullsocket(self):
+
+        self.assertRaises(AttributeError, loader.start_plugin, (dummy_plugin.server, None))
+
+
+    # Test the method start_plugin(class_to_run, socket_to_run): with a null inputs
+    # Excepted AttributeError since class_to_run does not have .start()
+    def test_startplugin_nullclass_to_run(self):
+        self.assertRaises(AttributeError, loader.start_plugin,(None, dummy_plugin.sock))
+
+    # Test the method start_plugin(class_to_run, socket_to_run): with an plugin invalid input
+    def test_startplugin_invalidplugin(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        try:
+
+            threading._start_new_thread(loader.start_plugin,(dummy_client.server, dummy_plugin.sock))
+
+            HOST = 'localhost'    # The remote host
+            PORT = 80              # The same port as used by the server
+
+            s.connect((HOST, PORT))
+            self.fail("Invalid class_to_run created a psuedo connection valid connection")
+        except Exception as e:
+            pass
+        finally:
+            s.close()
+
 
 
     def test_startplugins2(self):
