86b22892cb4eefe277f87929164969ca8927bfee
daemoniclegend <auroraaxela@gmail.com>
2016-02-09 15:58:01 -0700

    Current fixes to ssh_plugin. Currently has channel errors.

--- honeypot/tests/ssh_plugin_test.py
+++ honeypot/tests/ssh_plugin_test.py
@@ -6,7 +6,7 @@ import sys
 path = os.path.dirname(os.path.realpath(__file__)).replace("tests", "plugins")
 sys.path.insert(0, path)
 
-import ssh_plugin
+from plugins import ssh_plugin
 '''
 path = os.getcwd().replace('UnitTest', '')
 sys.path.insert(0, path)
