6f1bd1104a0d51684d1329c0c745798c66ffa593
Stephen Chavez <stephen.chavez12@gmail.com>
2016-02-13 16:01:39 -0700

    Made sure sys.exit was after lock releases... And made the lock var reachable from the exceptions

--- honeypot/honey_loader/loader.py
+++ honeypot/honey_loader/loader.py
@@ -4,36 +4,40 @@ import thread
 import sys
 import re
 
-#sets path for reading in .py files to plugins folder
+# sets path for reading in .py files to plugins folder
 path = os.path.dirname(os.path.realpath(__file__)).replace("honey_loader", "plugins")
 sys.path.insert(0, path)
 
-path = path.replace("plugins","")
+path = path.replace("plugins", "")
 
-text_file = open(path+"plugins.txt", "r")
-lines = re.split('\n| ',text_file.read()) #regex for new line and blanks
+text_file = open(path + "plugins.txt", "r")
+lines = re.split('\n| ', text_file.read())  # regex for new line and blanks
+lock = None
 
 
 def start_plugins():
-	try:
-		lock = threading.Lock()
-		for i in lines:
-			if i != '' and i[:1] != '#':		#ignore blank lines and comments starting with #
-				plugin = __import__(i)
-				plugin.server_plugin(lock)
+    global lock
+    try:
 
-		while True:
-			pass
-	except KeyboardInterrupt:
-		lock.acquire()
-		print '\nexiting via KeyboardInterrupt'
-		sys.exit()
-		lock.release()
-	except Exception as e:
-		lock.acquire()
-		print('ERROR: ' + str(e))
-		sys.exit()
-		lock.release()
+        lock = threading.Lock()
+        for i in lines:
+            if i != '' and i[:1] != '#':  # ignore blank lines and comments starting with #
+                plugin = __import__(i)
+                plugin.server_plugin(lock)
+
+        while True:
+            pass
+    except KeyboardInterrupt:
+        lock.acquire()
+        print '\nexiting via KeyboardInterrupt'
+        lock.release()
+        sys.exit()
+    except Exception as e:
+        lock.acquire()
+        print('ERROR: ' + str(e))
+        lock.release()
+        sys.exit()
+
 
 if __name__ == '__main__':
-	start_plugins()
+    start_plugins()
