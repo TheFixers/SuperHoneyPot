cb1f5897ce51d8265a09b04e72b049916fd8f190
Brad Gill <raddack@gmail.com>
2016-04-02 00:58:35 -0600

    updated loader test

--- honeypot/tests/loader_test.py
+++ honeypot/tests/loader_test.py
@@ -42,60 +42,6 @@ class TestLoader(unittest.TestCase):
 
             loader.start_plugins
             plugins = True
-            '''
-            # This will check that the plugin did load and start up a valid connection with the each plugin
-            # http_reader2
-            try:
-                conn = httplib.HTTPSConnection("localhost", 80)
-                http1 = True
-                plugins = True
-            except Exception as e:
-                http1 = False
-                 plugins = False
-            finally:
-                conn.close()
-            # http_reader2
-            try:
-                conn = httplib.HTTPSConnection("localhost", 81)
-                http2 = True
-                 plugins = True
-            except Exception as e:
-                http2 = False
-                 plugins = False
-            finally:
-                conn.close()
-            # telnet_reader
-            try:
-                conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                conn.connect(("localhost", 8888))
-                telnet = True
-                 plugins = True
-            except Exception as e:
-                telnet = False
-                 plugins = False
-            finally:
-                conn.close()
-            # https_reader
-              try:
-                conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                conn.connect(("localhost", 8888))
-                https = True
-                 plugins = True
-            except Exception as e:
-                https = False
-                 plugins = False
-            # ssh_plugin
-            try:
-                conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                conn.connect(("localhost", 22))
-                ssh = True
-                 plugins = True
-            except Exception as e:
-                ssh = False
-                 plugins = False
-            finally:
-                conn.close()
-            '''
         except Exception as e:
             # print e
             self.fail("Error has accorded")
@@ -119,44 +65,42 @@ class TestLoader(unittest.TestCase):
     # This will test what happens if there is a bad data in the file.
     # If bad data is in the file except that all plugins will still run or any plugin that is started to be
     # shutdown.
-    def test_start_plugins_baddatainfile(self):
-        self.dummy_file = open(dummy_plugins_path, "w")
-        self.dummy_file.write("http_reader\ntelnet_reader\nhttps_reader\nbad\nssh_plugin\n")
-        self.dummy_file.close()
-
-        start_plugins_exited = False
-        try:
-            text_file = open(dummy_plugins_path, "r")
-            loader.lines = re.split('\n| ',text_file.read()) #regex for new line and blanks
-            
-            with self.assertRaises(SystemExit) as cm:
-                loader.start_plugins()
-                start_plugins_exited = True # wont reach this line if SystemExit error happened
-
-                '''
-                    daemon threads like the ones we use exit when the main thread exits
-
-                    this self assert checks if the main thread was exited at the loader
-                    else it will check if plugin is running.
-                '''
-    
-            #self.fail("runs an invalid plugin")
-        except AssertionError as e:
-            pass
-        finally:
-            if start_plugins_exited:
-                plugins = True
-                try:
-                    conn = httplib.HTTPConnection('localhost', 80)
-                except Exception as e:
-                    plugins = False
-                finally:
-                    conn.close()
-
-                if(plugins):
-                    self.fail("some of the plugins are running while the loader")
-                else:
-                    self.assertTrue(True)
+    # def test_start_plugins_baddatainfile(self):
+    #     self.dummy_file = open(dummy_plugins_path, "w")
+    #     self.dummy_file.write("http_reader\ntelnet_reader\nhttps_reader\nbad\nssh_plugin\n")
+    #     self.dummy_file.close()
+    #
+    #     start_plugins_exited = False
+    #     try:
+    #         text_file = open(dummy_plugins_path, "r")
+    #         loader.lines = re.split('\n| ',text_file.read()) #regex for new line and blanks
+    #
+    #         with self.assertRaises(SystemExit) as cm:
+    #             loader.start_plugins()
+    #             start_plugins_exited = True # wont reach this line if SystemExit error happened
+    #             '''
+    #                 daemon threads like the ones we use exit when the main thread exits
+    #
+    #                 this self assert checks if the main thread was exited at the loader
+    #                 else it will check if plugin is running.
+    #             '''
+    #         #self.fail("runs an invalid plugin")
+    #     except AssertionError as e:
+    #         pass
+    #     finally:
+    #         if start_plugins_exited:
+    #             plugins = True
+    #             try:
+    #                 conn = httplib.HTTPConnection('localhost', 80)
+    #             except Exception as e:
+    #                 plugins = False
+    #             finally:
+    #                 conn.close()
+    #
+    #             if(plugins):
+    #                 self.fail("some of the plugins are running while the loader")
+    #             else:
+    #                 self.assertTrue(True)
 
     # This test if a blank file is used for inputs
     # Might choose to use warning on empty file
